b96d54850e6ec7406d173b25333842b6a1edb4d1
John Denver <johndenverbidong1@gmail.com>
2022-12-13 19:42:22 +0800

    remove purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,11 @@
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
-const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
   plugins: [
     tailwindcss(),
     autoprefixer(),
-    purgecss({
-      content: ["./public/index.html"],
-      css: ["./public/style.css"],
-    }),
     cssnano({
       preset: "default",
     }),
